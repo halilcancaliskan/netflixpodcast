db8bd872fbcd52673a8fde51f213a0af60f37d9b
Halil Can CALISKAN <61836866+halilcancaliskan@users.noreply.github.com>
2024-02-16 15:57:56 +0100

    title change

--- netflixpodcast/src/app/page.tsx
+++ netflixpodcast/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <header className="text-center mb-16">
-        <h1 className="text-4xl font-bold">Bienvenue sur Netflix Podcast</h1>
+        <h1 className="text-4xl font-bold">Bienvenue sur NetFlux Podcast</h1>
         <p className="mt-4 text-lg">
           Découvrez une bibliothèque de podcasts audio passionnants.
         </p>
@@ -23,27 +23,6 @@ export default function Home() {
             Écouter
           </button>
         </div>
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h2 className="text-xl font-semibold mb-2">Titre du Podcast</h2>
-          <p className="text-gray-600">
-            Description concise du podcast. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit.
-          </p>
-          <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-full">
-            Écouter
-          </button>
-        </div>
-        <div className="bg-white p-6 rounded-lg shadow-lg">
-          <h2 className="text-xl font-semibold mb-2">Titre du Podcast</h2>
-          <p className="text-gray-600">
-            Description concise du podcast. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit.
-          </p>
-          <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-full">
-            Écouter
-          </button>
-        </div>
-
         {/* Ajoutez d'autres aperçus de podcasts selon vos besoins */}
       </section>
 
