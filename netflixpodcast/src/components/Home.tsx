8611015bee65743750afb0608bb840d9a863fc83
Halil Can CALISKAN <61836866+halilcancaliskan@users.noreply.github.com>
2024-02-18 15:31:13 +0100

    front

--- netflixpodcast/src/components/Home.tsx
+++ netflixpodcast/src/components/Home.tsx
@@ -62,3 +62,76 @@ const Home = () => {
       }`;
     setTrailer(trailerURL);
   }, [movie]);
+  return (
+    <div className="bg-secondary relative px-4 md:px-0">
+
+      <div className="container mx-auto min-h-[calc(100vh-77px)] flex items-center relative">
+        <div className="flex-col lg:flex-row flex gap-10 lg:mx-10 py-20">
+          <div className="mx-auto flex-none relative">
+            <Image
+              src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`}
+              width={700}
+              height={700}
+              className="w-[300px] object-cover"
+              alt="movie poster"
+              priority
+              unoptimized  // Utilisez cette propriété pour contrôler le rendu côté serveur
+            />
+          </div>
+
+          <div className="space-y-6">
+            <div className="uppercase -translate-y-3 text-[26px] md:text-[34px] font-medium pr-4 text-white">
+              {movie?.title}
+            </div>
+            <div className="flex flex-col md:flex-row gap-2 md:gap-6">
+              <div>Durée : {movie?.runtime} MIN.</div>
+              <div>Avis : {movie?.vote_average} ⭐</div>
+            </div>
+
+            <div className="pt-14 space-y-2 pr-4">
+              <div>RÉSUMÉ :</div>
+              <div className="lg:line-clamp-4">{movie?.overview}</div>
+            </div>
+
+            <div
+              className="inline-block pt-6 cursor-pointer"
+              onClick={() => setShowPlayer(true)}
+            >
+              <div className="flex gap-2 items-center bg-white text-black px-4 py-2 mb-6">
+                Regarder la bande-annonce
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* React Player */}
+        <div
+          className={`absolute top-3 inset-x-[7%] md:inset-x-[13%] rounded overflow-hidden transition duration-1000 ${showPlayer ? "opacity-100 z-50" : "opacity-0 -z-10"
+            }`}
+        >
+          <div className="flex items-center justify-between bg-black text-[#f9f9f9] p-3.5">
+            <span className="font-semibold">Bande annonce</span>
+            <div
+              className="cursor-pointer w-8 h-8 flex justify-center items-center rounded-lg opacity-50 hover:opacity-75 hover:bg-[#0F0F0F]"
+              onClick={() => setShowPlayer(false)}
+            >
+              <IoMdClose className="h-5" />
+            </div>
+          </div>
+          <div className="relative pt-[56.25%]">
+            <ReactPlayer
+              url={trailer}
+              width="100%"
+              height="80%"
+              style={{ position: "absolute", top: "0", left: "0" }}
+              controls={true}
+              playing={showPlayer}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ netflixpodcast/src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const Navbar = () => {
+    const [input, setInput] = useState("");
+    const router = useRouter();
+
+    const searchMovie = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        router.push(`?movie=${input}`);
+        setInput("");
+    };
+
+    return (
+        <div className="bg-primary py-4 px-4 md:px-0">
+            <div className="container mx-auto flex justify-between items-center">
+                <Link href="/">
+                    <div className="text-[30px] font-medium">Netflux</div>
+                </Link>
+
+                <form onSubmit={searchMovie}>
+                    <div className="space-x-4">
+                        <input
+                            className="bg-secondary px-4 py-2 outline-none placeholder:text-textColor"
+                            type="text"
+                            value={input}
+                            placeholder="Rechercher un film"
+                            onChange={(e) => setInput(e.target.value)}
+                        />
+
+                        <button
+                            type="submit"
+                            className="bg-secondary text-textColor py-2 px-4 color: #00a400"
+                        >
+                            Rechercher
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
