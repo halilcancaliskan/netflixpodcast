061bfbb4d2b85c1869dfa8c2444e86cccf1169c2
Halil Can CALISKAN <61836866+halilcancaliskan@users.noreply.github.com>
2024-02-23 00:12:08 +0100

    suspense

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { Suspense, useState } from "react";
 import Image from "next/image";
 import Logo from "./TrailerVisionLogo.png";
 
@@ -16,31 +16,33 @@ const Navbar = () => {
   };
 
   return (
-    <div className="bg-black py-5 px-4 md:px-0">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link href="/">
-          <Image src={Logo} alt="Trailer Vision Logo" width={40} height={20} />
-        </Link>
-        <form onSubmit={searchMovie}>
-          <div className="space-x-4">
-            <input
-              className="bg-gray-200 px-4 py-2 outline-none placeholder:text-textColor text-gray-700 rounded-md"
-              type="text"
-              value={input}
-              placeholder="Rechercher un film"
-              onChange={(e) => setInput(e.target.value)}
-            />
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="bg-black py-5 px-4 md:px-0">
+        <div className="container mx-auto flex justify-between items-center">
+          <Link href="/">
+            <Image src={Logo} alt="Trailer Vision Logo" width={40} height={20} />
+          </Link>
+          <form onSubmit={searchMovie}>
+            <div className="space-x-4">
+              <input
+                className="bg-gray-200 px-4 py-2 outline-none placeholder:text-textColor text-gray-700 rounded-md"
+                type="text"
+                value={input}
+                placeholder="Rechercher un film"
+                onChange={(e) => setInput(e.target.value)}
+              />
 
-            <button
-              type="submit"
-              className="bg-red-500 text-gray-100  py-2 px-4 color: #00a400 rounded-md"
-            >
-              Rechercher
-            </button>
-          </div>
-        </form>
+              <button
+                type="submit"
+                className="bg-red-500 text-gray-100  py-2 px-4 color: #00a400 rounded-md"
+              >
+                Rechercher
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
